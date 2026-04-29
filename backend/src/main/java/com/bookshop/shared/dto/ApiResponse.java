4dd0e963147a9d10ddda34f66803506e93e5b4a5
Ahnis Singh <ahnisaneja@gmail.com>
2026-04-29 15:30:34 +0530

    feat: implement standard API response and error dto's

---
+++ backend/src/main/java/com/bookshop/shared/dto/ApiResponse.java
@@ -0,0 +1,22 @@
+package com.bookshop.shared.dto;
+
+import java.time.Instant;
+
+public record ApiResponse<T>(
+        boolean success,
+        String message,
+        T data,
+        Instant timestamp
+) {
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return new ApiResponse<>(true, message, data, Instant.now());
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, message, null,Instant.now());
+    }
+
+    public static <T> ApiResponse<T> error(T data, String message) {
+        return new ApiResponse<>(false, message, data, Instant.now());
+    }
+}

6897a526728494dd11ca147506e361041ae2d784
Ahnis Singh <ahnisaneja@gmail.com>
2026-04-29 23:19:26 +0530

    standardize error responses by updating global handlers and adding custom exceptions

---
+++ backend/src/main/java/com/bookshop/shared/exception/InvalidTokenException/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package com.bookshop.shared.exception.InvalidTokenException;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}

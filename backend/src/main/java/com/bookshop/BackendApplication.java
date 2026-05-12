a7efbc28704d91a518663a3f168fb5141725c16e
Ahnis Singh <ahnisaneja@gmail.com>
2026-05-12 21:12:32 +0530

    fixed sendgrid config issues

--- backend/src/main/java/com/bookshop/BackendApplication.java
+++ backend/src/main/java/com/bookshop/BackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = SesAutoConfiguration.class)
+@SpringBootApplication(exclude = {SesAutoConfiguration.class})
 @EnableAsync
 @EnableScheduling
 public class BackendApplication {

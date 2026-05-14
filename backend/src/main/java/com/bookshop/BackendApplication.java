f8854f97a33df68b89861662fe8627ed0f84dad3
Ahnis Singh <ahnisaneja@gmail.com>
2026-05-14 13:18:37 +0530

    Added aws docker logs driver

--- backend/src/main/java/com/bookshop/BackendApplication.java
+++ backend/src/main/java/com/bookshop/BackendApplication.java
@@ -10,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableScheduling
 public class BackendApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
